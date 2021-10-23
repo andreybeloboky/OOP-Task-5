a53bc3bf8a2d66d10d96949a49d3935879a9e45b
Andrey Beloboky <andrei.beloboky@yandex.ru>
2021-10-23 13:43:22 +0300

    add object and class: User and Product, create product string

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your user name.");
+        String userName = scanner.next();
+        System.out.println("Enter your password");
+        String password = scanner.next();
+        User user = new User(userName, password);
+        System.out.println("You are logged in as:" + " " + user.getLogin());
+        Product[] milkProducts = new Product[3];
+        Product[] fruitProducts = new Product[2];
+        String[] nameProductMilk = {"milk", "curd", "cheese"};
+        String[] nameProductFruit = {"apple", "mushrooms"};
+        int begin = 1;
+        int end = 10;
+        System.out.println("Enter number for further action. \n" + "Enter 1 - enter the screen list of products, another number - the end program.");
+        int number = scanner.nextInt();
     }
 }
---
+++ src/com/company/User.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class User {
+    private String login;
+    private String password;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+}
