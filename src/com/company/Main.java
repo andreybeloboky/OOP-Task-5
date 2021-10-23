387993d8e277dea24675cb56614d42257163867a
Andrey Beloboky <andrei.beloboky@yandex.ru>
2021-10-23 13:45:28 +0300

    add filling array Product and enter on screen list of product

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,5 +20,21 @@ public class Main {
         int end = 10;
         System.out.println("Enter number for further action. \n" + "Enter 1 - enter the screen list of products, another number - the end program.");
         int number = scanner.nextInt();
+        if (number == 1) {
+            System.out.println("List of products");
+            for (int i = 0; i < milkProducts.length; i++) {
+                milkProducts[i] = new Product(nameProductMilk[i], begin + (int) (Math.random() * end), begin + (int) (Math.random() * end));
+            }
+            for (int i = 0; i < fruitProducts.length; i++) {
+                fruitProducts[i] = new Product(nameProductFruit[i], begin + (int) (Math.random() * end), begin + (int) (Math.random() * end));
+            }
+            System.out.println("Milk products");
+            for (int i = 0; i < milkProducts.length; i++) {
+                System.out.println("name: " + milkProducts[i].getName() + " price: " + milkProducts[i].getPrice() + " rating: " + milkProducts[i].getRating());
+            }
+            System.out.println("Fruit products");
+            for (int i = 0; i < fruitProducts.length; i++) {
+                System.out.println("name: " + fruitProducts[i].getName() + " price: " + fruitProducts[i].getPrice() + " rating: " + fruitProducts[i].getRating());
+            }
     }
 }
