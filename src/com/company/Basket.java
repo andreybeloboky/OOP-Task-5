6e562008cd9722975720378f38fa2cd5311692ea
Andrey Beloboky <andrei.beloboky@yandex.ru>
2021-10-23 14:06:31 +0300

    create basket and function add product in basket at choice

---
+++ src/com/company/Basket.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Basket {
+    private String ListOfBoughtProducts;
+
+    public Basket(String ListOfBoughtProducts) {
+        this.ListOfBoughtProducts = ListOfBoughtProducts;
+    }
+
+    public String getListOfBoughtProducts() {
+        return ListOfBoughtProducts;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -36,5 +36,56 @@ public class Main {
             for (int i = 0; i < fruitProducts.length; i++) {
                 System.out.println("name: " + fruitProducts[i].getName() + " price: " + fruitProducts[i].getPrice() + " rating: " + fruitProducts[i].getRating());
             }
+            System.out.println("Would you like to buy something? \n" + "Enter 1 - yes, another number - the end program.");
+            int value = scanner.nextInt();
+            if (value == 1) {
+                System.out.println("How many products you want buy?");
+                int sizeBasket = scanner.nextInt();
+                Basket[] basket = new Basket[sizeBasket];
+                for (int i = 0; i < sizeBasket; i++) {
+                    System.out.println("How category will you want visit? \n" + "Enter 1 - milk products, 2 - fruit products.");
+                    int decide = scanner.nextInt();
+                    if (decide == 1) {
+                        System.out.println("Enter that add in basket: \n" + "1 - add milk, 2 - add curd, 3 - add cheese");
+                        int j = scanner.nextInt();
+                        switch (j) {
+                            case (1) -> {
+                                System.out.println("add milk");
+                                basket[i] = new Basket("apple");
+                            }
+                            case (2) -> {
+                                System.out.println("add curd");
+                                basket[i] = new Basket("curd");
+                            }
+                            case (3) -> {
+                                System.out.println("add cheese");
+                                basket[i] = new Basket("cheese");
+                            }
+                        }
+                    } else if (decide == 2) {
+                        System.out.println("Enter that add in basket: \n" + "1 - add apple, 2 - add mushrooms");
+                        int j = scanner.nextInt();
+                        switch (j) {
+                            case (1) -> {
+                                System.out.println("add apple");
+                                basket[i] = new Basket("apple");
+                            }
+                            case (2) -> {
+                                System.out.println("add mushrooms");
+                                basket[i] = new Basket("mushrooms");
+                            }
+                        }
+                    } else {
+                        System.out.println("Wrong enter!");
+                    }
+                }
+                System.out.println("List of products for bought");
+                for (int z = 0; z < basket.length; z++) {
+                    System.out.println(basket[z].getListOfBoughtProducts());
+                }
+            } else {
+                System.out.println("The end.");
+            }
+        }
     }
 }
